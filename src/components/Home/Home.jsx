cf966b3be5a562f0c6f1deb5e64adb5bc482b7bb
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-02-17 22:49:27 +0600

    install tailwind and daisy ui

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='bg-blue-400 text-5xl'>hello</h1>
+        </div>
+    );
+};
+
+export default Home;
