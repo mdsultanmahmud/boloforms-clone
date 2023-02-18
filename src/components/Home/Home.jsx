82880280ecf5e1bc2884dc25d7b05d311d65c7f2
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-02-18 09:38:35 +0600

    banner section design complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-4'>
             <Navbar></Navbar>
+            <Banner></Banner>
         </div>
     );
 };
